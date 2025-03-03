4e43ded767aec3575c85c07cca6b2bc15b8d1d6f
Alan Souza <alancesarsouza@gmail.com>
2025-03-03 13:11:01 -0300

    add basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col gap-4">
+      <h2 className="font-bold text-2xl ...">Regular</h2>
+      <br />
+      <div className="text-center">
+        <p className="text-xs ...">text-xs</p>
+        <p className="text-sm ...">text-sm</p>
+        <p className="text-base ...">text-base</p>
+        <p className="text-2xl ...">text-2xl</p>
+        <p className="text-3xl">text-3xl</p>
+        <p className="text-4xl">text-4xl</p>
+        <p className="text-5xl">text-5xl</p>
+        <p className="text-6xl">text-6xl</p>
+        <p className="text-7xl">text-7xl</p>
+        <p className="text-8xl">text-8xl</p>
+        <p className="text-9xl">text-9xl</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <h2 className="font-bold text-2xl ...">Semibold</h2>
+      <br />
+      <div className="text-center">
+        <p className="font-semibold text-xs ...">text-xs</p>
+        <p className="font-semibold text-sm ...">text-sm</p>
+        <p className="font-semibold text-base ...">text-base</p>
+        <p className="font-semibold text-2xl ...">text-2xl</p>
+        <p className="font-semibold text-3xl">text-3xl</p>
+        <p className="font-semibold text-4xl">text-4xl</p>
+        <p className="font-semibold text-5xl">text-5xl</p>
+        <p className="font-semibold text-6xl">text-6xl</p>
+        <p className="font-semibold text-7xl">text-7xl</p>
+        <p className="font-semibold text-8xl">text-8xl</p>
+        <p className="font-semibold text-9xl">text-9xl</p>
+      </div>
+      <h2 className="font-bold text-2xl ...">Bold</h2>
+      <br />
+      <div className="text-center">
+        <p className="font-bold text-xs ...">text-xs</p>
+        <p className="font-bold text-sm ...">text-sm</p>
+        <p className="font-bold text-base ...">text-base</p>
+        <p className="font-bold text-2xl ...">text-2xl</p>
+        <p className="font-bold text-3xl">text-3xl</p>
+        <p className="font-bold text-4xl">text-4xl</p>
+        <p className="font-bold text-5xl">text-5xl</p>
+        <p className="font-bold text-6xl">text-6xl</p>
+        <p className="font-bold text-7xl">text-7xl</p>
+        <p className="font-bold text-8xl">text-8xl</p>
+        <p className="font-bold text-9xl">text-9xl</p>
+      </div>
     </div>
   );
 }
