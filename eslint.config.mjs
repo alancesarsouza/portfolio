72df38d7e87ecf1cf148b93c7e6d016e29b07704
Alan Souza <alancesarsouza@gmail.com>
2025-03-06 01:08:12 -0300

    chore config lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,77 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['sort-exports'],
+    rules: {
+      'import/no-anonymous-default-export': [
+        'error',
+        {
+          allowArray: false,
+          allowArrowFunction: false,
+          allowAnonymousClass: false,
+          allowAnonymousFunction: false,
+          allowCallExpression: true,
+          allowNew: false,
+          allowLiteral: false,
+          allowObject: true
+        }
+      ],
+      'import/order': [
+        'error',
+        {
+          alphabetize: { caseInsensitive: false, order: 'asc' },
+          pathGroups: [
+            { pattern: 'react', group: 'external', position: 'before' },
+            { pattern: '@*', group: 'external', position: 'before' },
+            { pattern: '@/**', group: 'internal', position: 'before' },
+            { pattern: '*', group: 'internal', position: 'after' }
+          ],
+          'newlines-between': 'always',
+          groups: ['builtin', 'external', 'internal'],
+          pathGroupsExcludedImportTypes: ['react']
+        }
+      ],
+      'no-console': ['warn'],
+      'no-unneeded-ternary': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
+      'react/jsx-sort-props': [
+        1,
+        {
+          callbacksLast: true,
+          multiline: 'last',
+          reservedFirst: true,
+          shorthandFirst: true
+        }
+      ],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'sort-exports/sort-exports': [
+        /* https://www.npmjs.com/package/eslint-plugin-sort-exports */
+        'error',
+        { sortDir: 'asc' }
+      ]
+    }
+  }),
+  eslintConfigPrettier
 ];
 
 export default eslintConfig;
